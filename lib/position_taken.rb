441ce3eb715553cacd5a7f4cbfc82cc29517b6ee
Catherine Cordova <ccordova41@gmail.com>
2018-04-20 20:05:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,14 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || ""
-    false 
-  elsif board[index] == "X" || "O"
-    true 
-  end 
-end 
+#   if board[index] == " " || ""
+#     false 
+#   elsif board[index] == "X" || "O"
+#     true 
+#   end 
+# end 
 
  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
     false : true
+    
+    ens 
